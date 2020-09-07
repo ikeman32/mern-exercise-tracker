6a9029741ab07faf3a0632aae49a15897308a7a0
David Isakson <ikeman32@yahoo.com>
2020-09-06 21:58:39 -0700

    Full working app

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from './navbar.component.js';
+import ExerciseList from './exercise-list.component.js';
+import EditExercise from './edit-exercise.component.js';
+import CreateExercise from './create-exercise.component.js';
+import CreateUser from './create-user.component.js';
+
+export {
+    Navbar,
+    ExerciseList,
+    EditExercise,
+    CreateExercise,
+    CreateUser,
+};
