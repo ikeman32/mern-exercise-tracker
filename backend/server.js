dd49db280594341ca4cf564ebb1c154a2eefdb45
David Isakson <ikeman32@yahoo.com>
2020-09-06 19:39:46 -0700

    Exercuses schema

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true,
+useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', ()=>{
     console.log('MongoDB connection established successfully');
